e04a6931f44fa2da6aceec5d7e2b77442994c3f0
kiabq <joprograms@gmail.com>
2022-05-16 15:37:46 -0400

    ... commit thingy done because I forgot to delete commit info on .sh file :)

--- JS2TS.sh
+++ JS2TS.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-for file in *.js; do mv .js .ts; done
-=======
 ## Use this in cmd line to rename multiple files to TS.
 
 for file in *.ts; do mv "$file" "${file/.js/.tsx }"; done
->>>>>>> 36d71ea (Updated ts to tsx)
