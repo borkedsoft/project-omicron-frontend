ad358670abfcf8dada70b7f42d5c69c846ea6cf9
kiabq <joprograms@gmail.com>
2022-05-16 15:34:56 -0400

    Updated ts to tsx

--- JS2TS.sh
+++ JS2TS.sh
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 for file in *.js; do mv .js .ts; done
+=======
+## Use this in cmd line to rename multiple files to TS.
+
+for file in *.ts; do mv "$file" "${file/.js/.tsx }"; done
+>>>>>>> 36d71ea (Updated ts to tsx)
