1f4b0d0e88f324baaf46817fb1960fd7f3ae0719
My Name <foo@lisp.us.to>
2022-05-18 17:36:42 -0500

    Adding default code that draws on the canvas

--- omicron-frontend/src/App.tsx
+++ omicron-frontend/src/App.tsx
@@ -4,20 +4,52 @@ import './App.css';
 
 var state = {
     code: "",
-    evaled: function () {},
+    evaled: function (elapsed: number) {},
     running: false,
+    start: -1,
 };
 
-const animFrame = (timestamp: Number) => {
-    state.evaled();
+// TODO: should probably store, pass canvas context in as a parameter
+const initialCode =
+`var canvas = document.getElementById("gameCanvas");
+
+if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+
+    var t = elapsed/1000;
+    var nsin = 0.5 + 0.5*Math.sin(t);
+    var ncos = 0.5 + 0.5*Math.cos(t);
+    var x = (canvas.width  - 150)*(nsin);
+    var y = (canvas.height - 150)*(ncos);
+
+    var r = 64+Math.floor(191*nsin);
+    var g = 64+Math.floor(191*ncos);
+    var b = 127;
+
+    ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(25 + x, 25 + y, 100, 100);
+}`;
+
+const animFrame = (timestamp: number) => {
+    if (state.start < 0) {
+        state.start = timestamp;
+    }
+
+    state.evaled(timestamp - state.start);
 
     if (state.running) {
         requestAnimationFrame(animFrame);
     }
 }
 
+const loadCode = (str: string) => {
+    state.code = "(function(elapsed) {" + str + "})";
+    state.evaled = eval(state.code);
+}
+
 const updateCode = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    state.code = "(function() {" + e.target.value + "})";
+    state.code = "(function(elapsed) {" + e.target.value + "})";
 };
 
 const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -35,6 +67,8 @@ const stopAnim = (e: React.MouseEvent<HTMLButtonElement>) => {
 }
 
 const App: React.FunctionComponent = () => {
+  loadCode(initialCode);
+
   return (
     <div className="App">
         <h1>Working on TypeScript :)</h1>
@@ -42,15 +76,15 @@ const App: React.FunctionComponent = () => {
                   cols={45}
                   placeholder='Place Imaginary Code Here!'
                   onChange={updateCode}
+                  defaultValue={initialCode}
                   >
-        console.log("testing this");
         </textarea>
 
         <button onClick={handleClick}>Load</button>
         <button onClick={startAnim}>Start</button>
         <button onClick={stopAnim}>Stop</button>
 
-        {/* TODO: best way to choose dimensions? --> */}
+        {/* TODO: best way to choose dimensions? */}
         <canvas id="gameCanvas" width="800" height="450" style={{ border: "1px solid red" }}/>
     </div>
   );
