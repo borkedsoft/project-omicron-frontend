49a586040347579bbbe8a1221acf597ffe7b7c98
My Name <foo@lisp.us.to>
2022-05-24 19:28:42 -0500

    Refactoring App into a Component class

--- omicron-frontend/src/App.tsx
+++ omicron-frontend/src/App.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var state = {
-    code: "",
-    evaled: function (elapsed: number) {},
-    running: false,
-    start: -1,
-};
-
 // TODO: should probably store, pass canvas context in as a parameter
 const initialCode =
 `var canvas = document.getElementById("gameCanvas");
@@ -31,63 +24,104 @@ if (canvas.getContext) {
     ctx.fillRect(25 + x, 25 + y, 100, 100);
 }`;
 
-const animFrame = (timestamp: number) => {
-    if (state.start < 0) {
-        state.start = timestamp;
-    }
-
-    state.evaled(timestamp - state.start);
-
-    if (state.running) {
-        requestAnimationFrame(animFrame);
-    }
-}
-
-const loadCode = (str: string) => {
-    state.code = "(function(elapsed) {" + str + "})";
-    state.evaled = eval(state.code);
-}
-
-const updateCode = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    state.code = "(function(elapsed) {" + e.target.value + "})";
+type Properties = {
+    [name: string]: any
 };
 
-const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    state.evaled = eval(state.code);
-    console.log(state.code);
+interface AppState {
+    currentFile: string,
+    code:        string,
+    evaled:      (elapsed: number) => void,
+    running:     boolean,
+    start:       number,
+};
+
+export default class App extends React.Component<{}, AppState> {
+    constructor(props: Properties) {
+        super(props);
+
+        this.updateCode  = this.updateCode.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.startAnim   = this.startAnim.bind(this);
+        this.stopAnim    = this.stopAnim.bind(this);
+        this.animFrame   = this.animFrame.bind(this);
+
+        this.state = {
+            currentFile: "main.js",
+            code:        "",
+            evaled:      function (elapsed: number) {},
+            running:     false,
+            start:       -1,
+        };
+
+        // TODO: load main.js
+        //this.loadCode(initialCode);
+    }
+
+    render() {
+        return (
+          <div className="App">
+              <h1>Working on TypeScript :)</h1>
+              <textarea rows={10}
+                        cols={45}
+                        placeholder='Loading...'
+                        onChange={this.updateCode}
+                        defaultValue={initialCode}
+                        >
+              </textarea>
+
+              <button onClick={this.handleClick}>Load</button>
+              <button onClick={this.startAnim}>Start</button>
+              <button onClick={this.stopAnim}>Stop</button>
+
+              {/* TODO: best way to choose dimensions? */}
+              <canvas id="gameCanvas" width="800" height="450" style={{ border: "1px solid red" }}/>
+          </div>
+        );
+    }
+
+    animFrame(timestamp: number) {
+        if (this.state.start < 0) {
+            this.setState({
+                start: timestamp,
+            })
+        }
+
+        this.state.evaled(timestamp - this.state.start);
+
+        if (this.state.running) {
+            requestAnimationFrame(this.animFrame);
+        }
+    }
+
+    loadCode(str: string) {
+        let newcode = "(function(elapsed) {" + str + "})";
+
+        this.setState({
+            code:   newcode,
+            evaled: eval(newcode),
+        });
+    }
+
+    updateCode(e: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({
+            code: "(function(elapsed) {" + e.target.value + "})",
+        });
+    };
+
+    handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+        this.setState({ evaled: eval(this.state.code) });
+        console.log(this.state.code);
+    }
+
+    startAnim(e: React.MouseEvent<HTMLButtonElement>) {
+        this.setState({ running: true });
+        requestAnimationFrame(this.animFrame);
+    }
+
+    stopAnim(e: React.MouseEvent<HTMLButtonElement>) {
+        this.setState({ running: false });
+    }
 }
 
-const startAnim = (e: React.MouseEvent<HTMLButtonElement>) => {
-    state.running = true;
-    requestAnimationFrame(animFrame);
-}
-
-const stopAnim = (e: React.MouseEvent<HTMLButtonElement>) => {
-    state.running = false;
-}
-
-const App: React.FunctionComponent = () => {
-  loadCode(initialCode);
-
-  return (
-    <div className="App">
-        <h1>Working on TypeScript :)</h1>
-        <textarea rows={10}
-                  cols={45}
-                  placeholder='Place Imaginary Code Here!'
-                  onChange={updateCode}
-                  defaultValue={initialCode}
-                  >
-        </textarea>
-
-        <button onClick={handleClick}>Load</button>
-        <button onClick={startAnim}>Start</button>
-        <button onClick={stopAnim}>Stop</button>
-
-        {/* TODO: best way to choose dimensions? */}
-        <canvas id="gameCanvas" width="800" height="450" style={{ border: "1px solid red" }}/>
-    </div>
-  );
-}
-
-export default App;
+//export default App;
